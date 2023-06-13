3e01aa7c27a08358cce2383fcb501c07cd897885
Ian Hrydziuszko <132368002+robotsWhoSmokeCrack@users.noreply.github.com>
2023-06-12 22:05:33 -0400

    Add initial js code.

---
+++ controller.js
@@ -0,0 +1,52 @@
+const controller = (()=>{
+    let projects=[];
+    let toDoItems=[];
+
+    function addProject (project,toDoItems){
+        const proj = {
+            project : project,
+            toDoItems : toDoItems
+        }
+        projects.push(proj);
+        console.log(proj);}
+
+    function addToDo (title,dueDate,priority){
+        const toDo = {
+            title : title,
+            dueDate: dueDate,
+            priority:priority
+        }
+        toDoItems.push(toDo);
+        console.log(toDoItems)
+    }
+
+    function assignToDo(project,toDoItem){
+        let toDoIndex = toDoItems.findIndex(obj => obj.title == toDoItem);
+        let projIndex = projects.findIndex(obj => obj.project == project);
+        projects[projIndex].toDoItems.push(toDoItems[toDoIndex]);
+        console.log(projects[projIndex]);
+    }
+    
+    function removeToDo(project,toDoItem){
+        let toDoIndex = toDoItems.findIndex(obj => obj.title == toDoItem);
+        let projIndex = projects.findIndex(obj => obj.project == project);
+        projects[projIndex].toDoItems.splice(toDoItems[toDoIndex],1);
+        console.log(projects[projIndex]);
+    }
+
+    function changeProject(oldProj,newProj,toDoItem){
+        removeToDo(oldProj,toDoItem);
+        assignToDo(newProj,toDoItem);
+
+    }
+        return{
+            addProject,
+            addToDo,
+            assignToDo,
+            removeToDo,
+            changeProject
+    }
+
+} )();
+
+export{controller}
---
+++ index.js
@@ -0,0 +1,78 @@
+const toDoItem = (title,dueDate,priority) => {
+    controller.addToDo(title,dueDate,priority);
+    return {
+        title : title,
+        dueDate : dueDate,
+        priority : priority
+    }
+}
+
+const project = (projectName) =>{
+    let toDoItems = [];
+    controller.addProject(projectName, toDoItems);
+    return {
+        project: projectName,
+        toDoItems:[],
+    }
+}
+
+const controller = (()=>{
+    let projects=[];
+    let toDoItems=[];
+
+    function addProject (project,toDoItems){
+        const proj = {
+            project : project,
+            toDoItems : toDoItems
+        }
+        projects.push(proj);
+        console.log(proj);}
+
+    function addToDo (title,dueDate,priority){
+        const toDo = {
+            title : title,
+            dueDate: dueDate,
+            priority:priority
+        }
+        toDoItems.push(toDo);
+        console.log(toDoItems)
+    }
+
+    function assignToDo(project,toDoItem){
+        let toDoIndex = toDoItems.findIndex(obj => obj.title == toDoItem);
+        let projIndex = projects.findIndex(obj => obj.project == project);
+        projects[projIndex].toDoItems.push(toDoItems[toDoIndex]);
+        console.log(projects[projIndex]);
+    }
+    
+    function removeToDo(project,toDoItem){
+        let toDoIndex = toDoItems.findIndex(obj => obj.title == toDoItem);
+        let projIndex = projects.findIndex(obj => obj.project == project);
+        projects[projIndex].toDoItems.splice(toDoItems[toDoIndex],1);
+        console.log(projects[projIndex]);
+    }
+
+    function changeProject(oldProj,newProj,toDoItem){
+        removeToDo(oldProj,toDoItem);
+        assignToDo(newProj,toDoItem);
+
+    }
+        return{
+            addProject,
+            addToDo,
+            assignToDo,
+            removeToDo,
+            changeProject
+    }
+
+} )();
+
+project("myProject");
+
+project("myOtherProject");
+
+toDoItem("Get groceries","6/17/24","high");
+
+controller.assignToDo("myProject","Get groceries");
+
+controller.changeProject("myProject","myOtherProject","Get groceries");
