b2e51c1b32408cab9881ae1bc166272ecd95fb54
Ian Hrydziuszko <132368002+robotsWhoSmokeCrack@users.noreply.github.com>
2023-06-14 09:24:27 -0400

    Add drag and drop front end and functionality

--- src/index.js
+++ src/index.js
@@ -72,9 +72,22 @@ const controller = (()=>{
 } )();
 
 const viewEngine = (() => {
-    function cardBuild() {controller.projects.forEach((project)=>{
+    // function allowDrop(e){
+    //     e.preventDefault();
+    // }
+    // function drag(e){
+    //     e.dataTransfer
+    // }
+    function cardBuild() {
+        document.querySelector(".cards").innerHTML = "";
+        controller.projects.forEach((project)=>{
         const card = document.createElement("div");
         card.classList.add("card");
+        // card.addEventListener('dragenter', handleDragEnter);
+        // card.addEventListener('dragleave', handleDragLeave);
+        // card.addEventListener('drop', handleDrop);
+        // card.item.setAttribute('ondrop', 'drop(e)');
+        // card.item.setAttribute('ondragover', allowDrop(e));
 
         const proj = document.createElement("div");
         proj.classList.add("card-title");
@@ -85,6 +98,10 @@ const viewEngine = (() => {
         project.toDoItems.forEach((toDo)=>{
             const item = document.createElement("div");
             item.classList.add("to-do");
+            item.setAttribute('draggable', true);
+            // item.addEventListener('dragstart', handleDragStart);
+            // item.addEventListener('drop',handleDragEnd);
+            // item.setAttribute('ondragstart', drag(e));
             item.textContent = toDo.title
 
             card.appendChild(item)
@@ -96,10 +113,102 @@ const viewEngine = (() => {
     ) 
 
 }
-return{
-    cardBuild
-}
+
+        function newToDo(item){
+            const toDo = document.createElement('div');
+            toDo.classList.add("to-do");
+            toDo.setAttribute('draggable', true);
+            toDo.textContent = item;
+            document.querySelector(".sidebar").appendChild(toDo)
+        }
+        return{
+            cardBuild,
+            newToDo
+        }
  } )();
+
+//  card.addEventListener('dragenter', handleDragEnter);
+//  card.addEventListener('dragleave', handleDragLeave);
+//  card.addEventListener('drop', handleDrop);
+
+ (function dragDrop() {
+ let dragItem, listener 
+ 
+ dragItem = null;
+
+ listener = document.addEventListener;
+
+
+ listener('dragenter',function handleDragEnter(e){
+    e.target.classList.add('over');
+ });
+ 
+ listener('dragleave',function handleDragLeave (e){
+    e.target.classList.remove('over');
+ });
+
+ listener('dragend',function handleDragEnd (e){
+    e.target.classList.remove('over');
+ });
+
+ listener('dragover', function(e){
+    // console.log(`Dragover : ${e}`);
+    return e.preventDefault();
+
+ });
+
+ listener('dragstart',function handleDragStart(e){
+    dragItem = e.target
+    // e.dataTransfer.effectAllowed = 'move';
+    // e.dataTransfer.setData('text/html',this.outerHTML);
+    console.log(dragItem);
+    return dragItem;
+ });
+
+ listener('drop',function handleDrop(e){
+    e.preventDefault();
+    console.log(e.target.className);
+    if(e.target.className == "card over") {
+        console.log("dropzone");
+        console.log("old project is "+dragItem.parentNode.textContent + " new proj is " + e.target.textContent + " todo is " +dragItem.textContent);
+        if (dragItem.parentNode.className == "sidebar"){
+            controller.assignToDo(e.target.firstElementChild.textContent,dragItem.textContent);
+            dragItem.parentNode.removeChild(dragItem);
+            e.target.classList.remove("over")
+            return e.target.appendChild(dragItem);
+        } else {
+        // parentText(dragItem.parentNode);
+        // parentText(e.target);
+        controller.changeProject(dragItem.parentNode.firstElementChild.textContent,
+            e.target.firstElementChild.textContent,dragItem.textContent);
+        dragItem.parentNode.removeChild(dragItem);
+        e.target.classList.remove("over")
+        return e.target.appendChild(dragItem);
+        } 
+    } else {
+        if (e.target.className == "sidebar over"){
+            controller.removeToDo(dragItem.parentNode.firstElementChild.textContent.replace(dragItem.textContent,""),
+            dragItem.textContent);
+        dragItem.parentNode.removeChild(dragItem);
+        e.target.classList.remove("over")
+        return e.target.appendChild(dragItem);
+        }
+    }
+ });
+
+})();
+
+function parentText(el){
+    let text = "";
+for (let i = 0; i < el.childNodes.length; i++) {
+    let node = el.childNodes[i];
+    console.log({node})
+    if (node.nodeType  === Node.TEXT_NODE) {
+        text += node.data;
+    }
+}
+console.log("parent text is" +text);
+}
 
 project("Daily Things");
 
@@ -114,13 +223,40 @@ controller.changeProject("Daily Things","This Week","Get groceries");
 viewEngine.cardBuild();
 
 let newProj = document.querySelector(".nav>.new-project")
-newProj.addEventListener("click", function(){
-    const addProj = document.querySelector(".add-project");
-    addProj.classList.toggle("hidden");
+newProj.addEventListener("click", ()=>showThing(".add-project")
+)
 
+let newToDo = document.querySelector(".nav>.new-todo")
+newToDo.addEventListener("click", ()=>showThing(".add-to-do")
+)
+
+function showThing(thing){
+    document.querySelector(thing).classList.toggle("hidden");
+}
+
+document.getElementById("submit-project").addEventListener("click", function(){
+    project(document.querySelector("#project-name",).value);
+    showThing(".add-project");
+    viewEngine.cardBuild();
 });
 
+document.getElementById("submit-to-do").addEventListener("click", function(){
+    toDoItem(document.querySelector("#to-do-name").value,
+            document.querySelector("#to-do-date").value,
+            document.querySelector("#priority").value);
+    showThing(".add-to-do");
+    viewEngine.newToDo(document.querySelector("#to-do-name").value);
+});
 
-// document.getElementById("new-project").addEventListener("click", function(){
-//     controller.addProject(document.querySelector("#project-name").value);
-// });
+// document.addEventListener('DOMContentLoaded', (ev)=>{
+//     console.log(ev); 
+//     function handleDragStart(e){
+//         this.style.opacity = .4;
+//     }
+
+
+// let items = document.querySelectorAll('.to-do');
+// items.forEach(function(item){
+//     item.addEventListener('dragstart',handleDragStart());
+// })
+// })
