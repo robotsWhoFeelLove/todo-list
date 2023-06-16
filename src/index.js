76d8f8a1b2cfb6f3fa5bf2414246afc5fd196772
Ian Hrydziuszko <132368002+robotsWhoSmokeCrack@users.noreply.github.com>
2023-06-16 17:36:06 -0400

    Fixed remove task bug and updated stylings to be more modern

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import './style.css';
+import sticky from './sticky.png';
+import _, { forEach } from 'lodash';
 // import controller from './controller';
+
+document.querySelector(".header>div").addEventListener("click",(ev=>{
+    localStorage.clear()
+    console.log("cleared");
+}))
 
 const toDoItem = (title,dueDate,priority) => {
     controller.addToDo(title,dueDate,priority);
@@ -22,6 +29,12 @@ const project = (projectName) =>{
 const controller = (()=>{
     let projects =[];
     let toDoItems = [];
+
+    document.querySelector(".home").addEventListener("click",(e)=>{
+        console.log(projects);
+        console.log(toDoItems);
+    });
+
     if (getProj() != undefined){
         projects = getProj();
     } else {projects = []};
@@ -57,12 +70,29 @@ const controller = (()=>{
     }
     
     function removeToDo(project,toDoItem){
-        let toDoIndex = toDoItems.findIndex(obj => obj.title == toDoItem);
         let projIndex = projects.findIndex(obj => obj.project == project);
-        projects[projIndex].toDoItems.splice(toDoItems[toDoIndex],1);
-        saveAll();
-        console.log(projects[projIndex]);
+        console.log(projIndex);
+        let toDoIndex = projects[projIndex].toDoItems.findIndex(obj => obj.title == toDoItem);
+        console.log({toDoIndex})
+        projects[projIndex].toDoItems.splice(toDoIndex,1);
     }
+        // let i =0;
+        // projects.forEach( 
+        //     toDoItems.forEach((item)=>{
+        //     if(item.title == toDoItem){
+        //         let toDoIndex = toDoItems.findIndex(obj => obj.title == toDoItem)
+        //         projects[i.toDoItems].splice(toDoItems[toDoIndex],1)
+        //     }}
+        //     ))}
+        
+        // let toDoI = projects.find(obj => obj.title == toDoItem);
+        // let projIndex = projects.findIndex(obj => obj.project == project);
+    //     console.log(`Removing toDo: ${toDoItems[toDoIndex]} at Index ${toDoIndex} \n from Project: ${projects[projIndex]} at Index ${projIndex}`)
+    //     projects[projIndex].toDoItems.splice(toDoItems[toDoIndex],1);
+    //     _.omitBy(projects, _.isNil);
+    //     saveAll();
+    //     console.log(projects[projIndex]);
+    // }
 
     function removeUnassigned(toDoItem){
         let toDoIndex = toDoItems.findIndex(obj => obj.title == toDoItem);
@@ -83,9 +113,15 @@ const controller = (()=>{
 
     }
 
-    function checkDueToday(){
+    // function checkDueToday(){
 
-    }
+    // }
+    // function logToDo() {
+    //     const toDoString = JSON.stringify(toDoItems);
+    //    console.log(toDoString);
+        
+    // }
+    // logToDo();
         return{
             addProject,
             addToDo,
@@ -107,9 +143,18 @@ const viewEngine = (() => {
     // function drag(e){
     //     e.dataTransfer
     // }
+    
+    const stickyNote = new Image();
+    stickyNote.src = sticky;
+    document.querySelector(".header").appendChild(stickyNote);
     function cardBuild() {
+        let handledToDos = [];
         document.querySelector(".cards").innerHTML = "";
         document.querySelector(".sidebar").innerHTML = ""
+        const cleanProj = _.omitBy(controller.projects, _.isNil)
+        console.log(cleanProj)
+        // cleanProj.forEach((project)=>{
+      
         controller.projects.forEach((project)=>{
         const card = document.createElement("div");
         card.classList.add("card");
@@ -129,17 +174,19 @@ const viewEngine = (() => {
         card.appendChild(proj);
         card.appendChild(xOut);
 
-        project.toDoItems.forEach((toDo)=>{
 
+        
+        project.toDoItems.forEach((toDo)=>{
+            handledToDos.push(toDo);
             const item = document.createElement("div");
             item.classList.add("to-do");
             item.setAttribute('draggable', true);
             const toDoText = document.createElement('div');
-            toDoText.textContent = toDo.title
+            if(toDo.title)toDoText.textContent = toDo.title;
             const complete = document.createElement("input");
             complete.setAttribute('type', 'checkbox');
             const dueDate = document.createElement('div');
-            dueDate.textContent= toDo.dueDate;
+            dueDate.textContent= toDo.dueDate.substr(5);
             if(toDo.priority) item.classList.add("priority");
             item.classList.add(checkStatus(toDo.dueDate));
 
@@ -161,12 +208,18 @@ const viewEngine = (() => {
  
     }
     ) 
+    console.log({handledToDos});
     controller.toDoItems.forEach((item)=>{
-        if(!JSON.stringify(controller.projects).includes(item.title)){
-            newToDo(item.title);
+       if(!_.find(handledToDos,{"title": item.title})){
+        //     if(!project.toDoItems.includes(item)){console.log("Unassigned to do found: "+item.title)}
+        // })
+
+        // if(!_.find(controller.projects,{'toDoItems': item})){console.log("item found")}
+        // if(!JSON.stringify(controller.projects).includes(item.title)){
+            newToDo(item.title);}
         }
 
-    })
+    )
 }
 
         function newToDo(item){
@@ -175,7 +228,7 @@ const viewEngine = (() => {
             // toDo.setAttribute('draggable', true);
             // toDo.textContent = item;
             // document.querySelector(".sidebar").appendChild(toDo)
-            let toDoIndex = controller.toDoItems.findIndex(obj => obj.title == item);
+            let toDoIndex = controller.toDoItems.findIndex(obj => obj.title === item);
             const toDo = document.createElement("div");
             toDo.classList.add("to-do");
             toDo.setAttribute('draggable', true);
@@ -185,8 +238,8 @@ const viewEngine = (() => {
             complete.setAttribute('type', 'checkbox');
             if(controller.toDoItems[toDoIndex].priority) toDo.classList.add("priority")
             const dueDate = document.createElement('div');
-            dueDate.textContent= controller.toDoItems[toDoIndex].dueDate;
-           toDo.classList.add(checkStatus(dueDate.textContent));
+            dueDate.textContent= controller.toDoItems[toDoIndex].dueDate.substr(5);
+           toDo.classList.add(checkStatus(controller.toDoItems[toDoIndex].dueDate));
             
             // item.addEventListener('dragstart', handleDragStart);
             // item.addEventListener('drop',handleDragEnd);
@@ -223,23 +276,58 @@ const viewEngine = (() => {
             if(dueDate){ 
                 checkDate = flattenDate(dueDate)
             }
-            if(today == checkDate-1) return("yellow");
-            if(today > checkDate) return("red");
-            if(today <  checkDate-1) return("green");
-            if(today == checkDate) {
-                // dueToday = dueToday +1;
-                // document.querySelector(".due").textContent = dueToday;
-                return("dueToday")
-            }
+            if(today == checkDate) return("due-today");
+            if(today == checkDate-1) return("due-tomorrow");
+            if(today > checkDate-7) return("this-week");
+            if(today <  checkDate-6) return("green");
+            if(today > checkDate) return("over-due");
+            
         }
+        // function addClass (checker,someClass){
+        //     checker.forEach((thing)=>
+        //     thing.classList.Add(someClass))
+        // }
+        
+        function checkDue(todayPlusMinus,thing){
+            console.log(flattenDate(thing.dueDate)-todayPlusMinus);
+            console.log(flattenToday());
+              return(flattenDate(thing.dueDate)-todayPlusMinus<flattenToday())
+               }
+
+    //     function highlighter(int){
+    //         controller.toDoItems.forEach((toDo)=>{
+    //         console.log(toDo.title + " is due? " + checkDue(int,toDo))
+    //         if(checkDue(int,toDo)){console.log(toDo.title +" matched highlight");
+    //             document.querySelectorAll(".to-do").forEach((el)=>{
+    //                 console.log(el.textContent + el.firstElementChild.textContent);
+    //         if (el.firstElementChild.textContent == toDo.title){
+    //             el.classList.add("pulse");
+    //         }
+
+    //             })
+    //     }}) 
+    // }
+    // highlighter(1);
+
+        
+    function dueToday() {document.querySelectorAll(".due-today").forEach(el=>{
+        console.log(el)
+        el.classList.add("pulse")
+    })  }
+    dueToday();
+            
+        
 
         return{
             cardBuild,
             newToDo,
             checkStatus,
+            checkDue
             // dueToday
         }
  } )();
+
+
 
 //  card.addEventListener('dragenter', handleDragEnter);
 //  card.addEventListener('dragleave', handleDragLeave);
@@ -283,11 +371,12 @@ const viewEngine = (() => {
  listener('drop',function handleDrop(e){
     e.preventDefault();
     console.log(e.target.className);
-    if(e.target.className == "card over") {
+    if(e.target.classList.contains("card")) {
         console.log("dropzone");
         console.log("old project is "+dragItem.parentNode.textContent + " new proj is " + e.target.textContent + " todo is " +dragItem.textContent);
         if (dragItem.parentNode.className == "sidebar"){
-            controller.assignToDo(e.target.firstElementChild.textContent,dragItem.firstElementChild.textContent);
+            console.log(`assignToDo: ${e.target.firstChild.textContent} \n ${dragItem.firstChild.textContent}`)
+            controller.assignToDo(e.target.firstChild.textContent,dragItem.firstChild.textContent);
             dragItem.parentNode.removeChild(dragItem);
             e.target.classList.remove("over")
             return e.target.appendChild(dragItem);
@@ -301,7 +390,7 @@ const viewEngine = (() => {
         return e.target.appendChild(dragItem);
         } 
     } else {
-        if (e.target.className == "sidebar over"){
+        if (e.target.classList.contains("sidebar")){
             controller.removeToDo(dragItem.parentNode.firstElementChild.textContent,
             dragItem.firstElementChild.textContent);
         dragItem.parentNode.removeChild(dragItem);
@@ -315,24 +404,39 @@ const viewEngine = (() => {
 
 document.querySelector(".sidebar").onclick = function(ev){
     if(ev.target.parentNode.classList.contains("to-do")&& ev.target.checked){
-        controller.removeUnassigned(ev.target.parentNode.firstElementChild.textContent);
+        controller.removeUnassigned(ev.target.parentNode.firstChild.textContent);
         viewEngine.cardBuild();
         
     }
 }
 
+document.querySelector(".due").onclick = function(ev){
+    console.log("due clicked");
+    const list = document.querySelectorAll(".due-today");
+    console.log(list);
+    list.forEach((e)=>{
+        console.log(e);
+        e.classList.toggle("pulse");
+    })
+        
+    }
+
+
 document.querySelector(".cards").onclick = function(ev){
+    ev.stopPropagation();
     if(ev.target.classList.contains("x-out")){
-        controller.removeProject(ev.target.parentNode.firstElementChild.textContent);
+        controller.removeProject(ev.target.parentNode.firstChild.textContent);
         ev.target.parentNode.classList.add("push");
         setTimeout(()=>{
-            viewEngine.cardBuild()
+            viewEngine.cardBuild();
+            console.log("Removed Project " + ev.target.parentNode.firstChild.textContent);
         },400);
     }
     if(ev.target.parentNode.classList.contains("to-do") && ev.target.checked){
-        console.log(ev.target + ev.target.parentNode.textContent + ev.target.parentNode.parentNode.textContent)
-        controller.removeToDo(ev.target.parentNode.parentNode.firstElementChild.textContent,ev.target.parentNode.firstElementChild.textContent);
-        controller.removeUnassigned(ev.target.parentNode.firstElementChild.textContent); 
+        console.log("removing toDo: + "+ev.target.parentNode.firstChild.textContent + "\n From Project "+ev.target.parentNode.parentNode.firstChild.textContent)
+        controller.removeToDo(ev.target.parentNode.parentNode.firstChild.textContent,ev.target.parentNode.firstChild.textContent);
+        console.log("removing to Do: "+ ev.target.parentNode.firstChild.textContent + " from to do array");
+        controller.removeUnassigned(ev.target.parentNode.firstChild.textContent); 
         ev.target.parentNode.classList.add("shake")
         setTimeout(()=>{
             viewEngine.cardBuild()
@@ -341,17 +445,7 @@ document.querySelector(".cards").onclick = function(ev){
 }
 
 
-// function parentText(el){
-//     let text = "";
-// for (let i = 0; i < el.childNodes.length; i++) {
-//     let node = el.childNodes[i];
-//     console.log({node})
-//     if (node.nodeType  === Node.TEXT_NODE) {
-//         text += node.data;
-//     }
-// }
-// console.log("parent text is" +text);
-// }
+
 
 // project("Daily Things");
 
@@ -377,21 +471,34 @@ function showThing(thing){
     document.querySelector(thing).classList.toggle("hidden");
 }
 
-document.getElementById("submit-project").addEventListener("click", function(){
-    project(document.querySelector("#project-name",).value);
+document.querySelector(".new-project").addEventListener('submit', function(){
+    const projName = document.querySelector("#project-name");
+    if(_.find(controller.projects,{'project': projName.value.trim()})){
+        projName.setCustomValidity("This project already exists!")
+    } else {
+        projName.setCustomValidity("")
+    project(projName.value.trim());
     showThing(".add-project");
     viewEngine.cardBuild();
-});
+}});
 
-document.getElementById("submit-to-do").addEventListener("click", function(){
-    toDoItem(document.querySelector("#to-do-name").value,
+document.querySelector('.new-to-do').addEventListener('submit', function(){
+    const toDoName = document.querySelector("#to-do-name");
+    if(_.find(controller.toDoItems,{'title': toDoName.value.trim()})){
+        toDoName.setCustomValidity("This To Do already exists!");
+    } else {
+        toDoName.setCustomValidity("");
+    toDoItem(document.querySelector("#to-do-name").value.trim(),
             document.querySelector("#to-do-date").value,
             document.querySelector("#priority").checked)
             viewEngine.checkStatus(document.querySelector("#to-do-date").value);
     showThing(".add-to-do");
     viewEngine.cardBuild();
+    }
     // viewEngine.newToDo(document.querySelector("#to-do-name").value);
 });
+
+// document.querySelector('.new-to-do').addEventListener('submit',(ev) => {console.log(ev.target)})
 
 // document.addEventListener('DOMContentLoaded', (ev)=>{
 //     console.log(ev); 
@@ -405,6 +512,7 @@ document.getElementById("submit-to-do").addEventListener("click", function(){
 //     item.addEventListener('dragstart',handleDragStart());
 // })
 // })
+
 
 function saveAll (){
     localStorage.clear();
@@ -424,3 +532,25 @@ function getToDos (){
     let g = window.localStorage.getItem("toDoItems");
    if (g && g != "undefined") return JSON.parse(g);
 }
+
+
+
+
+// const toDos = new Set(controller.toDoItems.map(toDo=> toDo.title))
+// function findProj() { 
+//    if(_.find(controller.projects,{'project': 'Test Project'})){
+//     console.log("proj found")
+//    } else {
+//     console.log("no proj found")
+//    }
+// }
+// findProj();
+
+document.querySelector("#to-do-name").addEventListener("change",(e)=>{
+    e.target.setCustomValidity("");
+})
+document.querySelector("#project-name").addEventListener("change",(e)=>{
+    e.target.setCustomValidity("");
+})
+
+
